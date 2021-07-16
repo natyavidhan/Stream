549453427bb86e2341c5e7af966092d3420ed784
studiousgamer <natyavidhanbiswas10@gmail.com>
2021-07-17 00:16:40 +0530

    login with google added

--- app.py
+++ app.py
@@ -1,21 +1,52 @@
-from flask import Flask, render_template, redirect, request, session
+from flask import Flask, render_template, redirect, request, session, abort
 import pyrebase
 from werkzeug.utils import secure_filename
 import uuid
+import pathlib
 import json
 from pymongo import MongoClient
 import datetime
 import os
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+from google.oauth2 import id_token
+from google_auth_oauthlib.flow import Flow
+from pip._vendor import cachecontrol
+from flask_cors import CORS
+import google.auth.transport.requests
+import requests
 
 #-----------------------------------private stuff--------------------------
 
 with open("cred.json", "r") as f:
     credentials = json.load(f)
-    
+
+with open("creds.json", "r") as f:
+    keys = json.load(f)
+
+GOOGLE_CLIENT_ID = credentials['GOOGLE_CLIENT_ID']
+client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "creds.json")
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+flow = Flow.from_client_secrets_file(
+    client_secrets_file=client_secrets_file,
+    scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],
+    redirect_uri="http://127.0.0.1:5000/callback"
+)
+
+
+def login_is_required(function):
+    def wrapper(*args, **kwargs):
+        if "google_id" not in session:
+            return abort(401)  # Authorization required
+        else:
+            return function()
+
+    return wrapper
+
 creds = credentials['creds']
 db = MongoClient(credentials['mongo'])
 socialapp = db['Users']
-accounts = socialapp['users']
+accounts = socialapp['Users']
 posts = db['Posts']
 RAdb = db['RAdb']
 firebase = pyrebase.initialize_app(creds)
@@ -24,6 +55,11 @@ auth = firebase.auth()
 storage = firebase.storage()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = credentials['sessionkey']
+
+limiter = Limiter(
+    app,
+    key_func=get_remote_address
+)
 
 #----------------------------------------------------------------------------
 @app.route('/')
@@ -36,8 +72,7 @@ def index():
 @app.route('/profile')
 def home():
     if 'user' in session:
-        userposts = posts[session['user'][0]]
-        img = userposts.find({})
+        img = RAdb['posts'].find({'by': session['user'][0]})#userposts.find({})
         data = []
         total = 0
         for image in img:
@@ -50,67 +85,49 @@ def home():
             image = os.path.join(os.path.dirname(__file__), "static\\user.png")
             storage.child(f"users/{session['user'][0]}/Info/ProfilePic.png").put(image) 
             pfp = storage.child(f"users/{session['user'][0]}/Info/ProfilePic.png").get_url(None)  
-        return render_template('user home.html', images = data, user = session['user'][0], total=total, pic=pfp)
+        return render_template('user home.html', images = data, user = session['user'][2], total=total, pic=pfp)
     else:
         return render_template('home.html')
 
-@app.route('/login')
+@app.route("/login")
 def login():
-    return render_template('login.html')
+    authorization_url, state = flow.authorization_url()
+    session["state"] = state
+    return redirect(authorization_url)
 
-@app.route('/login', methods=['POST'])
-def login_func():
-    email = request.form['user']
-    password = request.form['password']
-    try:
-        user = auth.sign_in_with_email_and_password(email, password)
-        print(user)
-        name = accounts.find_one({"email":email})
-        username = name['username']
-        session['user'] = [username, email, password]
+@app.route("/callback")
+def callback():
+    flow.fetch_token(authorization_response=request.url)
+
+    if not session["state"] == request.args["state"]:
+        abort(500)  # State does not match!
+
+    credentials = flow.credentials
+    request_session = requests.session()
+    cached_session = cachecontrol.CacheControl(request_session)
+    token_request = google.auth.transport.requests.Request(session=cached_session)
+
+    id_info = id_token.verify_oauth2_token(
+        id_token=credentials._id_token,
+        request=token_request,
+        audience=GOOGLE_CLIENT_ID
+    )
+    email = id_info.get("email")
+    name = id_info.get("name")
+    # return str([email, name, picture])
+    ifExist = accounts.find_one({'email': email})
+    if ifExist is not None:
+        session['user'] = [ifExist['_id'], email, name]
         return redirect('/profile')
-    except:
-        return "Wrong Email or/and Password!"
-    
-@app.route('/signup')
-def signup():
-    return render_template('signup.html')
+    else:
+        ID = str(uuid.uuid4())
+        data = {'_id': ID, 'email': email, 'name': name}
+        accounts.insert_one(data)
+        session['user'] = [ID, email, name]
+        image = os.path.join(os.path.dirname(__file__), "static\\user.png")
+        storage.child(f"users/{session['user'][0]}/Info/ProfilePic.png").put(image) 
+        return redirect('/profile')
 
-@app.route('/signup', methods=['POST'])
-def signup_func():
-    email = request.form['email']
-    name = request.form['username']
-    password = request.form['password']
-    conpassword = request.form['confirmpassword']
-    
-    try:
-        if password == conpassword:
-            detailsifany = accounts.find_one({"email":email})
-            if detailsifany == None:
-                nameifany = accounts.find_one({"username":name})
-                if nameifany == None:
-                    user = auth.create_user_with_email_and_password(email, password)
-                    print(user)
-                    data = {
-                        "username": name,
-                        "email": email,
-                        "password": password
-                    }
-                    usercollection = socialapp[name]
-                    usercollection.insert_one(data)
-                    accounts.insert_one(data)
-                    session['user'] = [name, email, password]
-                    image = os.path.join(os.path.dirname(__file__), "static\\user.png")
-                    storage.child(f"users/{session['user'][0]}/Info/ProfilePic.png").put(image)          
-                    return redirect('/profile')
-                else:
-                    return "Username Already Exist!"
-            else:
-                return "Email Already Exist!"
-        else:
-            return "passwords doesn't match!"
-    except:
-        return "Error (weak password)"
 
 @app.route('/upload')
 def upload():
@@ -132,6 +149,7 @@ def upload_func():
     data = {
         '_id': filename,
         'by' : session['user'][0],
+        'name': session['user'][2],
         'url': url, 
         'text': text, 
         'tags': taglist,
@@ -140,8 +158,8 @@ def upload_func():
         'comments': [],
         'likes': []
         }
-    collection = posts[session['user'][0]]
-    collection.insert_one(data)
+    # collection = posts[session['user'][0]]
+    # collection.insert_one(data)
     RAdb['posts'].insert_one(data)
     for tag in taglist:
         TagExist = RAdb['tags'].find_one({'_id': tag})
@@ -169,14 +187,13 @@ def logout():
 
 @app.route('/user/<string:name>')
 def users(name):
-    nameifany = accounts.find_one({"username":name})
+    nameifany = accounts.find_one({"_id":name})
     if nameifany != None:
         if 'user' in session:
             if name == session['user'][0]:
                 return redirect('/profile')
             else:
-                userposts = posts[name]
-                img = userposts.find({})
+                img = RAdb['posts'].find({'by': name})
                 data = []
                 total = 0
                 for image in img:
@@ -184,7 +201,8 @@ def users(name):
                     data.append({"ID": image['_id'], "link": image['url'], "text": image['text'], "date": image['date'], "time": image['time']})
                 # print(data)
                 pfp = storage.child(f"users/{name}/Info/ProfilePic.png").get_url(None)  
-                return render_template('profiles.html', images = data, user = name, total=total, pic=pfp)
+                name = accounts.find_one({'_id': name})
+                return render_template('profiles.html', images = data, user = name['name'], total=total, pic=pfp)
         else:
             return redirect('/login')
     else:
@@ -193,22 +211,25 @@ def users(name):
 @app.route('/post/<string:postID>')
 def post(postID):
     # try:
-    pfp = storage.child(f"users/{session['user'][0]}/Info/ProfilePic.png").get_url(None) 
+    post = RAdb['posts'].find_one({'_id': postID})
+    user = post['by']
+    pfp = storage.child(f"users/{user}/Info/ProfilePic.png").get_url(None) 
     
-    return render_template('post.html', data=RAdb['posts'].find_one({'_id': postID}), pic=pfp)
+    return render_template('post.html', data=post, pic=pfp)
     # except:
     #     return "Post With this ID doesn't exist!"
 @app.route('/comment/<string:ID>/')
-def comemnt(ID):
+@limiter.limit("5 per minute")
+def commemnt(ID):
     if 'user' in session:
         text = request.args.get('text')
-        data = {'by': session['user'][0], 'text': text}
+        data = {'by': session['user'][2], 'text': text, 'id': session['user'][0]}
         try:
             comments = RAdb['posts'].find_one({'_id': ID})
             if comments is not None:
                 comments = comments['comments']
                 comments.append(data)
-                RAdb['posts'].update({'_id': ID}, {'$set': {'comments': comments}})
+                RAdb['posts'].update_one({'_id': ID}, {'$set': {'comments': comments}})
                 return "Commented!"
             else:
                 return "that ID doesn't exist!"
@@ -218,15 +239,4 @@ def comemnt(ID):
         return redirect('/login')
                 
 if __name__ == '__main__':
-    app.run(port=8080, debug=True)
-#fizz buzz for fun
-for int i in 100:
-    else if i % 5 == 0:
-        print("fizz")
-    else if i % 3 == 0:
-        print("buzz")
-    else if i % 3 == 0 && i % 5 == 0:
-        print("fizzbuzz")
-    else:
-        print(str(i))
-        
+    app.run(debug=True)
